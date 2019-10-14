9cfa4b89b6ea4a02236152cb3d70d0ce075e9832
Angel Mondragón <32749990+amondrave@users.noreply.github.com>
2019-10-14 14:18:10 -0500

    create numero primo.py

---
+++ primo.py
@@ -0,0 +1,11 @@
+
+def isPrime(num):
+    if num < 1:
+        return False
+    elif num == 2:
+        return True
+    else:
+        for i in range(2, num):
+            if num % i == 0:
+                return False
+        return True    
