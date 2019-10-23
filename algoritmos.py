1a407cfa88a47e154b2b5108963a4cec80035cc4
nicolaspinzonaparicio <56931067+nicolaspinzonaparicio@users.noreply.github.com>
2019-10-23 11:26:37 -0500

    update
    update

--- algoritmos.py
+++ algoritmos.py
@@ -63,3 +63,18 @@ x = int(input("Digite el numero: "))
 y = getMayor(x)
 
 print("EL numero mayor formado es: "+str(y))
+numeros = []
+
+# Le agregamos 3 números
+for i in range(3):
+  numero = float(input("Introduce el número #{}: ".format(i + 1)))
+  numeros.append(numero)
+
+# Asumir que el mayor es el primero de la lista
+mayor = numeros[0]
+# Recorrer y comparar
+for numero in numeros:
+    if numero > mayor:
+        mayor = numero
+# Imprimir el resultado
+print("Mayor:", mayor)
